66bc4519748305bf2b138ddc2569668a2e8457da
bitblocks-dev <83256901+bitblocks-dev@users.noreply.github.com>
2026-03-24 22:07:00 -0400

    Initial functionality

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Instant;
+
+use bevy::{
+    app::{App, Plugin},
+    time::Time,
+};
+
+pub mod prelude {
+    #[allow(unused)]
+    pub use crate::InGame;
+}
+
+///
+pub struct GameTimePlugin;
+
+impl Plugin for GameTimePlugin {
+    fn build(&self, app: &mut App) {}
+}
+
+#[allow(unused)]
+#[derive(Debug)]
+pub struct InGame {
+    last_external_time: Instant,
+}
+
+impl Default for InGame {
+    fn default() -> Self {
+        Self {
+            last_external_time: Instant::now(),
+        }
+    }
+}
+
+#[allow(unused)]
+trait InGameTime {
+    fn update_from_external(&mut self, instant: Instant);
+}
+
+impl InGameTime for Time<InGame> {
+    fn update_from_external(&mut self, instant: Instant) {
+        let delta = instant - self.context().last_external_time;
+        self.advance_by(delta);
+        self.context_mut().last_external_time = instant;
+    }
 }
